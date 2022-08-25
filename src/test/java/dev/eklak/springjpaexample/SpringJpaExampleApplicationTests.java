c60f4a2e46b5c589cedaf2c7de2d4c4d0ef20d30
Eklak Dangaura <azens1995@gmail.com>
2022-08-25 11:57:32 +0545

    Initialize the project with required dependencies

---
+++ src/test/java/dev/eklak/springjpaexample/SpringJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eklak.springjpaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
