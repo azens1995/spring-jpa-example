53f3077a73a059a1602178421380dbb78371c954
Eklak Dangaura <azens1995@gmail.com>
2022-08-26 13:13:32 +0545

    Add command line runner app to fetch the data

--- src/main/java/dev/eklak/springjpaexample/SpringJpaExampleApplication.java
+++ src/main/java/dev/eklak/springjpaexample/SpringJpaExampleApplication.java
@@ -1,13 +1,25 @@
 package dev.eklak.springjpaexample;
 
+import dev.eklak.springjpaexample.repositories.PersonJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJpaExampleApplication {
+public class SpringJpaExampleApplication implements CommandLineRunner {
+
+    @Autowired
+    private PersonJpaRepository personJpaRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJpaExampleApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        var person = personJpaRepository.findById(10001);
+        System.out.println("The person with ID 10001 is " + person.toString());
+    }
 }
