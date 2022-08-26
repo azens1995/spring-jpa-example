6c4bf1bbc9a6ddaecfc7265264d3ce586a0d646d
Eklak Dangaura <azens1995@gmail.com>
2022-08-26 13:12:52 +0545

    Add script to insert initial data

---
+++ src/main/java/dev/eklak/springjpaexample/repositories/PersonJpaRepository.java
@@ -0,0 +1,2 @@
+package dev.eklak.springjpaexample.repositories;public class PersonJpaRepository {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- Insert data into the person table
+INSERT INTO person (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES (10001, 'Eklak', 'Mahendranagar', CURRENT_DATE());
+INSERT INTO person (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES (10002, 'Romeo', 'Lalitpur', CURRENT_DATE());
+INSERT INTO person (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES (10003, 'Raskin', 'Sanagaun', CURRENT_DATE());
+INSERT INTO person (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES (10004, 'Sushil', 'Machhegaun', CURRENT_DATE());
