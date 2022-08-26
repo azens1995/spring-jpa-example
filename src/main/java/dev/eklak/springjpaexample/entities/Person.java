a29f3971a2c7f06b7b5448f6d9e5ca5bf006c6b5
Eklak Dangaura <azens1995@gmail.com>
2022-08-26 13:13:17 +0545

    Create entity and repository to access the data layer

--- src/main/java/dev/eklak/springjpaexample/entities/Person.java
+++ src/main/java/dev/eklak/springjpaexample/entities/Person.java
@@ -1,2 +1,69 @@
-package dev.eklak.springjpaexample.entities;public class Person {
+package dev.eklak.springjpaexample.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+// Annotate class with @Entity
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String location;
+    private Date birthDate;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, String location, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
--- src/main/java/dev/eklak/springjpaexample/repositories/PersonJpaRepository.java
+++ src/main/java/dev/eklak/springjpaexample/repositories/PersonJpaRepository.java
@@ -1,2 +1,18 @@
-package dev.eklak.springjpaexample.repositories;public class PersonJpaRepository {
+package dev.eklak.springjpaexample.repositories;
+
+import dev.eklak.springjpaexample.entities.Person;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class PersonJpaRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Person findById(Integer id) {
+        return entityManager.find(Person.class, id);
+    }
 }
